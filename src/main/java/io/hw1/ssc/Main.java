60ffc6313861e56e4b206a181b97abffc78a59a0
embo Dormo <welcomeadam34@gmail.com>
2024-05-09 19:50:02 +0700

    first commit

--- src/main/java/io/hw1/ssc/Main.java
+++ src/main/java/io/hw1/ssc/Main.java
@@ -2,6 +2,7 @@ package io.hw1.ssc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!_testing");
     }
+
 }
