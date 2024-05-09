f7c125a50600f2af50a8560661ba02b0a38c1bb9
embo Dormo <welcomeadam34@gmail.com>
2024-05-09 20:01:53 +0700

    first commit

--- src/main/java/io/hw1/ssc/Main.java
+++ src/main/java/io/hw1/ssc/Main.java
@@ -1,8 +1,10 @@
 package io.hw1.ssc;
+import io.muzoo.ssc.assignment.tracker.SscAssignment;
 
-public class Main {
+public class Main extends SscAssignment {
     public static void main(String[] args) {
         System.out.println("Hello world!!!_testing");
+        System.out.println("First Commit, testing");
     }
 
 }
