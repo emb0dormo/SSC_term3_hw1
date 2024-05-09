0cb362144d11838e0bee9eeec85c9b137a4e6bb1
embo Dormo <welcomeadam34@gmail.com>
2024-05-09 21:24:21 +0700

    updated commit

--- src/main/java/io/hw1/ssc/Main.java
+++ src/main/java/io/hw1/ssc/Main.java
@@ -8,18 +8,88 @@
 package io.hw1.ssc;
 
 import io.muzoo.ssc.assignment.tracker.SscAssignment;
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import java.util.Set;
+
+class MyFileVisitor extends SimpleFileVisitor<Path> {
+    private int totalFiles = 0;
+    private int totalDirs = 0;
+    private int totalDuplicateFiles = 0;
+    private final Set<String> specialExtensions = new HashSet<>();
+
+    public int getTotalFiles() {return totalFiles;}
+    public int getTotalDirs() {return totalDirs;}
+    public int getTotalDuplicateFiles() {return totalDuplicateFiles;}
+    public Set<String> getSpecialExtensions() {return specialExtensions;}
+
+    private String getFileExtension(Path file) {
+        String fileName = file.getFileName().toString();
+        return fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        if (attrs.isDirectory()){
+            totalDirs++;
+        }
+        return FileVisitResult.CONTINUE;
+    }
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (attrs.isRegularFile()){
+            totalFiles++;
+
+            String fileExtension = getFileExtension(file);
+            if (!specialExtensions.add(fileExtension)){
+                totalDuplicateFiles++;
+            }
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
 
 public class Main extends SscAssignment {
     public static void main(String[] args) {
         Options options = new Options();
-        options.addOption("a", "count duplicates", false, "print total number of duplicates");
+        options.addOption("c", "count duplicates", false, "print total number of duplicates");
         options.addOption("a", "algo", false, "Specifies the algorithm for finding duplicates");
         options.addOption("p", "print", false, "Print relative pathes of all duplicates grouped together");
-        options.addOption("f", "path to folder", false, "Specifies the path to the target folder (required)");
+        options.addOption("f", "path to folder", true, "Specifies the path to the target folder (required)");
 
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine commandLine = parser.parse(options,args);
+            String folderPath= commandLine.getOptionValue("f");
+            if(folderPath==null){
+                System.out.println("Folder path not specified");
+                printHelp(options);
+                return;
+            }
+            Path folderPathObj = Paths.get(folderPath);
+            if(!Files.exists(folderPathObj) || !Files.isDirectory(folderPathObj)){
+                System.out.println("Folder does not exist");
+                printHelp(options);
+                return;
+            }
+            MyFileVisitor fileVisitor = new MyFileVisitor();
+            Files.walkFileTree(folderPath, fileVisitor);
+            if(commandLine.hasOption("c")){
+                System.out.println("total number of  duplicates "+ fileVisitor.getTotalDuplicateFiles());
+            }
+        } catch (ParseException | IOException e) {
+            System.out.println("Error: "+ e.getMessage());
+            printHelp(options);
+        }
+    }
 
+    private static void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java -jar ssc-y23t3-dir-walker.jar", options);
     }
 
 }
