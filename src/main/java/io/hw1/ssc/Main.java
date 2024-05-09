823c4a79fb9c6672726d4bdda7eb05d2e2de05ff
embo Dormo <welcomeadam34@gmail.com>
2024-05-09 20:30:37 +0700

    first commit

--- src/main/java/io/hw1/ssc/Main.java
+++ src/main/java/io/hw1/ssc/Main.java
@@ -1,12 +1,25 @@
+// references:
+//  https://commons.apache.org/proper/commons-cli/usage.html
+// https://www.baeldung.com/java-file-extension#:~:text=java%E2%80%9C.,returns%20extension%20of%20the%20filename.
+// https://commons.apache.org/proper/commons-cli/apidocs/org/apache/commons/cli/HelpFormatter.html
+// https://mkyong.com/java/java-get-the-name-or-path-of-a-running-jar-file/
+// https://docs.oracle.com/javase/8/docs/api/java/nio/file/FileVisitor.html
+
 package io.hw1.ssc;
+
 import io.muzoo.ssc.assignment.tracker.SscAssignment;
+import org.apache.commons.cli.Options;
+
 
 public class Main extends SscAssignment {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!_testing");
-        System.out.println("First Commit, testing");
-        System.out.println("First Commit, testing");
-        System.out.println("second Commit, testing");
+        Options options = new Options();
+        options.addOption("a", "count duplicates", false, "print total number of duplicates");
+        options.addOption("a", "algo", false, "Specifies the algorithm for finding duplicates");
+        options.addOption("p", "print", false, "Print relative pathes of all duplicates grouped together");
+        options.addOption("f", "path to folder", false, "Specifies the path to the target folder (required)");
+
+
     }
 
 }
